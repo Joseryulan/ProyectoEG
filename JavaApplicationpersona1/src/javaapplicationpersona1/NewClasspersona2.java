878795a4047acc6653d8821e93bd6b66e5fad52a
sena <sena@sena-MS-7817>
2017-04-26 08:51:38 -0500

    usando el trabjo en equipo

--- JavaApplicationpersona1/src/javaapplicationpersona1/NewClasspersona2.java
+++ JavaApplicationpersona1/src/javaapplicationpersona1/NewClasspersona2.java
@@ -10,5 +10,17 @@ package javaapplicationpersona1;
  * @author sena
  */
 public class NewClasspersona2 {
+    private String id;
+     public void setid( String idParametro){
+        this.id= idParametro;
+     }      
+    
+    public String getId() {
+        return this.id;         
+    }  
+    public void andar () {
+                System.out.print(id + "esta caminando");
+                
+            }
     
 }
