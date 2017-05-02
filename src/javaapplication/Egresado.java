29e16913368a9a6a3d9aec250322c40e678da4dc
sena <sena@sena-MS-7817>
2017-05-02 11:30:52 -0500

    usando el trabjo en equipo 1

---
+++ src/javaapplication/Egresado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+/**
+ *
+ * @author sena
+ */
+public class Egresado {
+    private String idEgresado;
+    public void setIdEgresado(String idEgresadoP){
+        this.idEgresado= idEgresadoP;
+                   
+    }
+    public String getIdEgresado(){
+        return idEgresado;
+    }
+    public void solicitar(){}
+}
